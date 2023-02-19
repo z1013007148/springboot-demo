1600bf5516d2542b387cabffe6dc2efd70b3273d
z1013007148 <1013007148@qq.com>
2023-02-19 14:27:00 +0800

    init

---
+++ src/main/java/com/zhao/project/mapper/UserMapper.java
@@ -0,0 +1,15 @@
+package com.zhao.project.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.zhao.project.model.entity.User;
+
+/**
+ * @Entity com.zhao.project.model.domain.User
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
